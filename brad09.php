dba88bc564416a1026945a828db2d694a875d948
Brad Chao <brad@brad.tw>
2024-01-12 10:10:05 +0800

    01121010

---
+++ brad09.php
@@ -0,0 +1,15 @@
+<?php
+    $x = $y = $r = '';
+    if (isset($_GET['x']) && isset($_GET['y'])) {
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        $r = $x + $y;    
+    }
+?>
+<form>
+    <input name="x" value="<?php echo $x; ?>" />
+    +
+    <input name="y" value="<?php echo $y; ?>" />
+    <input type="submit" value="="/>
+    <span><?php echo $r; ?></span>
+</form>
