e9987926623eb5c92449826c010043ce29916ed9
Brad Chao <brad@brad.tw>
2024-01-15 12:09:11 +0800

    01151209

---
+++ brad14.php
@@ -0,0 +1,19 @@
+<?php
+    $ary = array(1,2,'brad',4,12.3,true);
+    var_dump($ary);
+    echo '<hr />';
+    for ($i = 0; $i <= 5; $i++){
+        echo "{$ary[$i]}<br />";
+    }
+    echo '<hr />';
+    echo $ary[3];
+    echo '<hr />';
+    echo count($ary);
+    echo '<hr />';
+    $ary[3] = 'iSpan';
+    for ($i = 0; $i < count($ary); $i++){
+        echo "{$ary[$i]}<br />";
+    }
+    echo '<hr />';
+    var_dump($ary);
+?>
