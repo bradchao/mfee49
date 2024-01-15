551732a8c3450f345b662a7ff8e6f646dd0c2b93
Brad Chao <brad@brad.tw>
2024-01-15 16:31:07 +0800

    01151630

--- pokerv4.php
+++ pokerv4.php
@@ -3,11 +3,36 @@
     shuffle($poker);
     
     foreach($poker as $v){
-        echo "{$v}<br />";
+        //echo "{$v}<br />";
     }
-    echo '<hr />';
+
     $players = [[],[],[],[]];
     foreach($poker as $index => $card){
         $players[$index % 4][(int)($index / 4)] = $card;
     }
+
+
 ?>
+<hr />
+<table border="1" width="100%">
+    
+    <?php
+        foreach($players as $player){
+            echo '<tr>';
+            foreach($player as $card){
+                echo "<td>{$card}</td>";
+            }
+            echo '</tr>';
+        }
+    ?>
+    
+</table>
+&spades; &hearts; &diams; &clubs;
+
+
+
+
+
+
+
+
