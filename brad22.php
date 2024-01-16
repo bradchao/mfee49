76daa5785bd660cef01462e5602a10fc3cd2c56a
Brad Chao <brad@brad.tw>
2024-01-16 14:27:11 +0800

    01161427

--- brad22.php
+++ brad22.php
@@ -1,10 +1,26 @@
 <?php
-    // 1. 畫布
+    $rate = 0;
+    if (isset($_GET['rate'])){
+        $rate = $_GET['rate'];     // 50%
+    }
+    
 
+    // 1. 畫布
+    $img = ImageCreate(400, 20);
+    // echo gettype($img);
+    
     // 2. 作畫
+    $yellow = ImageColorAllocate($img, 255, 255, 0);
+    $red = ImageColorAllocate($img, 255, 0, 0);
+
+    ImageFilledRectangle($img, 0, 0, 400, 20, $yellow);
+    ImageFilledRectangle($img, 0, 0, 400*$rate/100, 20, $red);
 
     // 3. 輸出 => Browser, File
+    header('content-type: image/jpeg');
+    ImageJpeg($img);
 
     // 4. 清除
+    imagedestroy($img);
 
 ?>
---
+++ brad24.php
@@ -0,0 +1,12 @@
+<?php
+    $img = imagecreatefromjpeg('imgs/coffee.jpg');
+
+    $blue = imagecolorallocate($img, 0, 0, 255);
+    imagettftext($img, 36, -30, 100, 200, $blue, 
+        'fonts/brad.ttf', '您好, 全世界');
+
+    header('content-type: image/jpeg');
+    imagejpeg($img);
+
+    imagedestroy($img);
+?>
