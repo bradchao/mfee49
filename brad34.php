f1206b727e9e7468f4c4fea0cd67cdc3ba872a5e
Brad Chao <brad@brad.tw>
2024-01-17 15:46:43 +0800

    01171546

--- brad34.php
+++ brad34.php
@@ -1,7 +1,12 @@
 <?php
     $mysqli = new mysqli('localhost', 'root', '', 'ispan', 3306);
     //var_dump($mysqli);
-
-    $mysqi->query();
+    $sql = 'INSERT INTO cust (cname,tel,birthday) ' .
+            'VALUES ("brad","123","1999-01-02")';
+    if ($mysqli->query($sql)){
+        echo 'OK';
+    }else{
+        echo 'XX';
+    }
 
 ?>
