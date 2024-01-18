6f9ea7322b68ead145c0919c57a33f4cc96f080a
Brad Chao <brad@brad.tw>
2024-01-18 11:52:40 +0800

    01181152

--- brad39.php
+++ brad39.php
@@ -1,14 +1,19 @@
+<meta charset="UTF-8" />
 <?php
 
     $mysqli = new mysqli('localhost', 'root', '', 'ispan', 3306);
     $mysqli->set_charset('utf8');
 
-    $sql = 'SELECT id, name, addr, city, town, picurl FROM food ' .
-        'WHERE name LIKE ? OR addr LIKE ? OR city LIKE ? OR town LIKE ?';
+    $sql = 'SELECT id, name, addr, city, town, picurl FROM food';
+    if (isset($_GET['key'])){
+        $sql .= ' WHERE name LIKE ? OR addr LIKE ? OR city LIKE ? OR town LIKE ?';
 
-    $key = '%中正%';
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('ssss', $key, $key, $key, $key);
+        $key = "%{$_GET['key']}%";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('ssss', $key, $key, $key, $key);
+    }else{        
+        $stmt = $mysqli->prepare($sql);
+    }
 
     if ($stmt->execute()){
         $stmt->store_result();
@@ -16,6 +21,10 @@
 ?>
 <h1>Brad Big Company</h1>
 <hr />
+<form>
+    <input name='key' />
+    <input type="submit" value="搜尋" />
+</form>
 <table border="1" width="100%" >
     <tr>
         <th>#</th>
@@ -23,6 +32,7 @@
         <th>Address</th>
         <th>City</th>
         <th>Town</th>
+        <th>Photo</th>
     </tr>
     <?php
         while ($stmt->fetch()){
@@ -32,6 +42,7 @@
             echo "<td>{$addr}</td>";
             echo "<td>{$city}</td>";
             echo "<td>{$town}</td>";
+            echo "<td><img src='{$picurl}' width='160px' height='90px'></td>";
             echo '</tr>';
         }
     ?>
