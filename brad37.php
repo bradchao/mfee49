de5bac513acb2cfb9d91e9cebc01be69ff614be7
Brad Chao <brad@brad.tw>
2024-01-18 10:18:12 +0800

    01181018

--- brad37.php
+++ brad37.php
@@ -6,8 +6,19 @@
     $data = json_decode($json);
     //var_dump($data);
 
-    foreach($data as $row){
-        echo "{$row->ID} : {$row->Name} : {$row->Tel}<br />";
-    }
+    $mysqli = new mysqli('localhost', 'root', '', 'ispan', 3306);
+    $mysqli->set_charset('utf8');
 
+    $sql = 'INSERT INTO food (foodid,name,addr,feature,city,town,picurl,lat,lng) ' .
+            'VALUES (?,?,?,?,?,?,?,?,?)';
+    $stmt = $mysqli->prepare($sql);
+
+    foreach($data as $row){
+        $stmt->bind_param('sssssssdd',$row->ID,$row->Name,
+        $row->Address,$row->FoodFeature,$row->City,$row->Town,$row->PicURL,
+        $row->Latitude,$row->Longitude);
+        $stmt->execute();
+        //echo "{$row->ID} : {$row->Name} : {$row->Tel}<br />";
+    }
+    echo 'OK';
 ?>
