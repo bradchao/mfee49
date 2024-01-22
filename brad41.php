ffc2a48735c87f84999885604576e5e3f02f6e95
Brad Chao <brad@brad.tw>
2024-01-22 14:12:28 +0800

    01221412

--- brad41.php
+++ brad41.php
@@ -1,6 +1,8 @@
-<form action='brad42.php' method="post">
+<form action='brad42.php' method="post"
+    enctype="multipart/form-data">
     Account: <input name='account' /><br />
     Password: <input type="password" name='passwd' /><br />
     Name: <input name='name' /><br />
+    Icon: <input type="file" name="icon" /><br />
     <input type="submit" value='Register' />
 </form>
--- brad42.php
+++ brad42.php
@@ -5,11 +5,16 @@
     $passwd = password_hash($_REQUEST['passwd'], PASSWORD_DEFAULT);
     $name = $_REQUEST['name'];
 
-    $sql = 'INSERT INTO member (account,passwd,name) VALUES (?,?,?)';
+    $icon = $_FILES['icon'];
+    $iconData = file_get_contents($icon['tmp_name']);
+    $iconType = $icon['type'];
+
+    $sql = 'INSERT INTO member (account,passwd,name, icon, icontype)' .
+        ' VALUES (?,?,?,?,?)';
     $mysqli = new mysqli('localhost','root','', 'ispan', 3306);
     $mysqli->set_charset('utf8');
     $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('sss', $account,$passwd, $name);
+    $stmt->bind_param('sssss', $account,$passwd, $name, $iconData, $iconType);
     if ($stmt->execute()){
         echo 'success';
     }else{
